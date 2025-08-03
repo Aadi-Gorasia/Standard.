2b9d1b1ce0c3f057442694eb9b344fa7ff101d54
gorasiaAadi <aadi.gorasia6@gmail.com>
2025-08-03 16:53:45 +0530

    Configure WhiteNoise for static files

--- nonprofit_site/settings.py
+++ nonprofit_site/settings.py
@@ -1,25 +1,17 @@
 # nonprofit_site/settings.py
 
 from pathlib import Path
-import os # NEW: Import the os module
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# We will move this to an environment variable in a future step for better security
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-!)ec9cl_m5faw8(878e0bl0%!0ykb#_v0^_t*5&*3^zzc*fzag')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# Render will set DEBUG to False automatically.
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-# THE FIX IS HERE
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
-# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -32,9 +24,10 @@ INSTALLED_APPS = [
     'builder',
 ]
 
-# ... (the rest of your settings file remains the same)
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    # THE FIX IS HERE: Add WhiteNoise middleware right after security.
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -42,6 +35,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 ROOT_URLCONF = 'nonprofit_site.urls'
 TEMPLATES = [ { 'BACKEND': 'django.template.backends.django.DjangoTemplates', 'DIRS': [BASE_DIR / 'templates'], 'APP_DIRS': True, 'OPTIONS': { 'context_processors': [ 'django.template.context_processors.debug', 'django.template.context_processors.request', 'django.contrib.auth.context_processors.auth', 'django.contrib.messages.context_processors.messages', ], }, }, ]
 WSGI_APPLICATION = 'nonprofit_site.wsgi.application'
@@ -51,7 +45,19 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
+
 STATIC_URL = 'static/'
+
+# THE FIX IS HERE: These lines configure Django and WhiteNoise for static files.
+# This tells Django where to collect all static files into a single folder.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# This tells Django to use WhiteNoise's optimized storage backend.
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashboard' 
 LOGOUT_REDIRECT_URL = 'core:homepage'
