fd19ea525d4dc4baef45d2c39af9f0f71ffb1b97
gorasiaAadi <aadi.gorasia6@gmail.com>
2025-08-03 16:44:52 +0530

    Configure ALLOWED_HOSTS for production

--- nonprofit_site/settings.py
+++ nonprofit_site/settings.py
@@ -1,12 +1,25 @@
 # nonprofit_site/settings.py
 
 from pathlib import Path
+import os # NEW: Import the os module
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = 'django-insecure-YOUR-UNIQUE-SECRET-KEY-HERE'
-DEBUG = True
-ALLOWED_HOSTS = []
 
+# SECURITY WARNING: keep the secret key used in production secret!
+# We will move this to an environment variable in a future step for better security
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-!)ec9cl_m5faw8(878e0bl0%!0ykb#_v0^_t*5&*3^zzc*fzag')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# Render will set DEBUG to False automatically.
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+
+# THE FIX IS HERE
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -14,13 +27,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # Our Apps
     'core',
     'accounts',
-    'builder', # <-- NEW: Register our builder app
+    'builder',
 ]
 
+# ... (the rest of your settings file remains the same)
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -30,27 +42,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'nonprofit_site.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
+TEMPLATES = [ { 'BACKEND': 'django.template.backends.django.DjangoTemplates', 'DIRS': [BASE_DIR / 'templates'], 'APP_DIRS': True, 'OPTIONS': { 'context_processors': [ 'django.template.context_processors.debug', 'django.template.context_processors.request', 'django.contrib.auth.context_processors.auth', 'django.contrib.messages.context_processors.messages', ], }, }, ]
 WSGI_APPLICATION = 'nonprofit_site.wsgi.application'
-
 DATABASES = { 'default': { 'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3', } }
 AUTH_PASSWORD_VALIDATORS = [ { 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', }, { 'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', }, { 'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', }, { 'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', }, ]
 LANGUAGE_CODE = 'en-us'
@@ -59,6 +53,5 @@ USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_REDIRECT_URL = 'dashboard' 
 LOGOUT_REDIRECT_URL = 'core:homepage'
